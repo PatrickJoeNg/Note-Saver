c1ec437386b20a58dbb6578cf39a800b17025c09
Patrick Ng <itzsparty@gmail.com>
2020-12-28 14:48:08 -0500

    changed application form
    Changed application from Console app to window form application. woops

---
+++ NoteSaver/NoteSaverForm.Designer.cs
@@ -0,0 +1,52 @@
+﻿
+namespace NoteSaver
+{
+    partial class NoteSaverForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // NoteSaverForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(867, 690);
+            this.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Blue;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "NoteSaverForm";
+            this.Text = "NoteSaverForm";
+            this.Load += new System.EventHandler(this.NoteSaverForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
